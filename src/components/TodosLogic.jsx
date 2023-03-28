51dc02cb110de16028f9487f9de0dda431c7afd5
Nahom <zerihunnahom@gmail.com>
2023-03-28 11:19:48 +0300

    Add edit todo functionality

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -45,10 +45,27 @@ const TodosLogic = () => {
     };
     setTodos([...todos, newTodo]);
   };
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo, title: updatedTitle,
+          };
+        }
+        return todo;
+      }),
+    );
+  };
   return (
     <div>
       <InputTodo addTodoItem={addTodoItem} />
-      <TodosList todosProps={todos} handleChange={handleChange} delTodo={delTodo} />
+      <TodosList
+        todosProps={todos}
+        handleChange={handleChange}
+        delTodo={delTodo}
+        setUpdate={setUpdate}
+      />
     </div>
   );
 };
